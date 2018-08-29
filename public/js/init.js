a460e314c5c732bd3c279823c5d3096c351400db
brenofranca <breno.franca@lipperhub.com>
2018-08-29 08:47:17 -0300

    Insert page teste and categories in page

--- public/js/init.js
+++ public/js/init.js
@@ -6,4 +6,26 @@
     $('.modal').modal();
 
   }); // end of document ready
+
+	$(window).load(function() {
+	  var valor1 = 1;
+	  var link = "http://pokeapi.co/api/v2/pokemon/" + valor1;
+	  $.ajax({
+	      url: link,
+	      type: 'GET',
+	      dataType: 'json',
+
+	    })
+	    .done(function() {
+	      console.log("success");
+	      console.log(valor1);
+	      document.getElementById("demo").innerHTML = valor1;
+	    })
+	    .fail(function() {
+	      console.log("error");
+	    })
+	    .always(function() {
+	      console.log("complete");
+	    });
+	});
 })(jQuery); // end of jQuery name space
